edbe124817c1683c5e36c858f9fb55727013332a
Higor Araujo <higoraraujobezerra@gmail.com>
2020-04-28 11:29:16 -0300

    Very bad solution

---
+++ src/application/Programm.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entitites.Reservation;
+
+public class Programm {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkin = sdf.parse(sc.next());
+		
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkout = sdf.parse(sc.next());
+		
+		if(!checkout.after(checkin)) {
+			System.out.println("Error in reservation: Ckeck-out must be after check-in date");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkin, checkout);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.print("Enter data to update the reservation: ");
+			
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkin = sdf.parse(sc.next());
+			
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkout = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			
+			if(checkin.before(now) || checkout.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future");
+			}
+			else if(!checkout.after(checkin)) {
+				System.out.println("Error in reservation: Ckeck-out must be after check-in date");
+			}
+			else {
+				reservation.updatesDates(checkin, checkout);
+				System.out.println("Resertion: "+ reservation);
+			}
+			
+		}
+		
+		sc.close();
+	}
+
+}
